af8d01938694ad6966ddfcfc1cc59354b8ae83e4
SALMA IMASSENDA <salmaimassenda2023@gmail.com>
2024-11-03 11:36:27 +0100

    TP ORM

---
+++ src/main/java/com/example/tporm/TpOrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.tporm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpOrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpOrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tporm/TpOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tporm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpOrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
